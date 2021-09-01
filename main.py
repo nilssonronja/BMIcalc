fda3f6513a21228dbffbcbde856fa601a2d21625
Ronja <77671505+nilssonronja@users.noreply.github.com>
2021-09-01 12:06:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+name = 'YK' 
+height_m=1.5 
+weight_kg=45 
+bmi = weight_kg / (height_m*height_m)
+print (bmi) 
+22.5 
+print ('bmi:') 
+bmi:
+if bmi < 25: 
+    print (name) 
+    print ('is not overweight') 
+else: 
+    print (name)  
+    print ('is overweight')  
